17ed5d32954114b3ca75bec6eda371fcec731cc1
Andrei Kolupaev <deaddev0784@gmail.com>
2023-08-22 12:31:50 +0400

    Разработана логика кнопок главного меню

---
+++ Assets/Scripts/UI/UI_Buttons_Settings.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class UI_Buttons_Settings : MonoBehaviour
+{
+    [SerializeField] private GameObject o_menu;
+    [SerializeField] private GameObject o_volume;
+    [SerializeField] private GameObject o_video;
+    [SerializeField] private GameObject o_graphic;
+    [SerializeField] private GameObject o_controls;
+
+    private void DisableAllAdditionalWindows()
+    {
+        o_volume.SetActive(false);
+        o_video.SetActive(false);
+        o_graphic.SetActive(false);
+        o_controls.SetActive(false);
+    }
+
+    public void Back_OnClick()
+    {
+        o_menu.SetActive(true);
+        DisableAllAdditionalWindows();
+        gameObject.SetActive(false);
+    }
+
+    public void Volume_OnClick()
+    {
+        DisableAllAdditionalWindows();
+        o_volume.SetActive(true);
+    }
+
+    public void Video_OnClick()
+    {
+        DisableAllAdditionalWindows();
+        o_video.SetActive(true);
+    }
+
+    public void Graphic_OnClick()
+    {
+        DisableAllAdditionalWindows();
+        o_graphic.SetActive(true);
+    }
+
+    public void Controls_OnClick()
+    {
+        DisableAllAdditionalWindows();
+        o_controls.SetActive(true);
+    }
+}
