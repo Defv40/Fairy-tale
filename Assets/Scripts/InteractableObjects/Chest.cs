e09f520b3a20599b3731e08bbc7c7031c1b55ecf
German Popov <ProgDefv@gmail.com>
2023-08-24 14:29:17 +0400

    Добавлен звук взаимодействия с сундуком

--- Assets/Scripts/InteractableObjects/Chest.cs
+++ Assets/Scripts/InteractableObjects/Chest.cs
@@ -1,19 +1,22 @@
 
 using UnityEngine;
 
+[RequireComponent (typeof(AudioSource))]
+[RequireComponent(typeof(Animator))]
 public class Chest : InteractableObject
 {
     private Animator _animator;
 
     private void Awake()
     {
+        _audioSource = GetComponent<AudioSource>();
         _animator = GetComponent<Animator>();
     }
 
     public override void Interact()
     {
         _animator.SetTrigger("Interact");
-
+        _audioSource.Play();
         gameObject.layer = LayerMask.NameToLayer("Default");
     }
 }
