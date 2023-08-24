7b1635803a4680e14805d08403895a83e7cd7f74
Andrei Kolupaev <deaddev0784@gmail.com>
2023-08-24 19:59:11 +0400

    Фикс расширении в настройках меню

--- Assets/Scripts/UI/UI_Controls_Settings_Video.cs
+++ Assets/Scripts/UI/UI_Controls_Settings_Video.cs
@@ -1,28 +1,35 @@
-
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
-
+using UnityEngine.Rendering;
 
 public class UI_Controls_Settings_Video : MonoBehaviour
 {
-    [SerializeField] private TMP_Dropdown resolution;
+    [SerializeField] private TMP_Dropdown tmp_resolution;
+    private Resolution[] resolutions;
 
     private void Start()
     {
-        // ¬ –≈À»«≈ –¿¡Œ“¿≈“ Õ≈  Œ––≈ “ÕŒ!
-        for (int i = 0; i < Screen.resolutions.Length; i++)
-        {
-            string strResolution = $"{Screen.resolutions[i].width}x{Screen.resolutions[i].height}";
-            resolution.options.Add(new TMP_Dropdown.OptionData(strResolution));
+        resolutions = (from r in Screen.resolutions where r.refreshRateRatio.Equals(Screen.currentResolution.refreshRateRatio) select r).ToArray();
 
-            if (Screen.resolutions[i].Equals(Screen.currentResolution)) resolution.value = i;
+        int i = -1;
+        while (++i < resolutions.Length)
+        {
+            var str = $"{resolutions[i].width}x{resolutions[i].height}";
+            tmp_resolution.options.Add(new TMP_Dropdown.OptionData(str));
+
+            if (resolutions[i].Equals(Screen.currentResolution)) 
+                tmp_resolution.value = i;
         }
     }
 
     public void Resolution_DropDown_OnChanged(int value)
     {
-        var selected = Screen.resolutions[value];
-        if(selected.Equals(Screen.currentResolution)) return;
+        var selected = resolutions[value];
+        if (selected.Equals(Screen.currentResolution)) return;
 
         Screen.SetResolution(selected.width, selected.height, true);
         Global_Settings.Init.resolution = selected;
@@ -30,7 +37,7 @@ public class UI_Controls_Settings_Video : MonoBehaviour
 
     public void VSync_Toggle_OnChanged(bool value)
     {
-        if(value) Global_Settings.Init.vsync = 1;
+        if (value) Global_Settings.Init.vsync = 1;
         else Global_Settings.Init.vsync = 0;
     }
 }
